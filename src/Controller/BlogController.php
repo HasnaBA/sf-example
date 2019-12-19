2272ce70c3a1863b247a1d58ad428be49cb18e6b
b <h.bourajoini@gmail.com>
2019-12-19 14:27:59 +0100

    ajout article

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BlogController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+       
+
+        return $this->render('blog/home.html.twig');
+    }
+}
